a1c739c101a9e3bb18d5ee0eabe09b4ce267c731
austinechisutia <austineakhonya624@gmail.com>
2025-02-07 09:07:29 +0300

    second commit

--- main.js
+++ main.js
@@ -1,2 +1,43 @@
-let character = "Hello";
-console.log(character)
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+button1.onclick = goToStore();
+button2.onclick = goCave();
+button3.onclick = fightDragon()
+
+
+function goToStore(){
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+
+}
+
+function goCave(){
+    console.log("Going to cave");  
+}
+
+function fightDragon(){
+    console.log("Fighting Dragon");  
+}
+
+
+
