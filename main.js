3d51b947b497d421ae15a7c33290e3b03738c9f2
austinechisutia <austineakhonya624@gmail.com>
2025-02-10 10:13:24 +0300

    added sevaral functions

--- main.js
+++ main.js
@@ -19,7 +19,14 @@ button3.onclick = fightDragon;
 
 
 function goStore(){
-    console.log("Going to store");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store."
+
     
 }
 
@@ -30,5 +37,13 @@ function goCave(){
 
 function fightDragon(){
     console.log("Fight the Dragon");
+    
+}
+function buyHealth(){
+
+}
+function buyWeapon(){
+    
+}function goTown(){
     
 }
