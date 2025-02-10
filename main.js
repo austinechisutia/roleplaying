e350b228fc1f6b8d3db6f9bea106a1829cae71ce
austinechisutia <austineakhonya624@gmail.com>
2025-02-10 11:37:27 +0300

    arrays and objects 57

--- main.js
+++ main.js
@@ -17,7 +17,15 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-
+function goTown(){
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight Dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\"."
+}
 function goStore(){
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
@@ -25,7 +33,7 @@ function goStore(){
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
-    text.innerText = "You enter the store."
+    text.innerText = "You enter the store.";
 
     
 }
@@ -43,7 +51,5 @@ function buyHealth(){
 
 }
 function buyWeapon(){
-    
-}function goTown(){
     
 }
